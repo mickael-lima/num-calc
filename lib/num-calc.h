8e4bddcfc0e037063d456a0a629a670746f996ae
Mickael Lima <smoek@tutanota.com>
2025-03-02 13:51:54 -0300

    `[bisection]` add more conditions for `root_interval` to be effectably used
    - `[fix]:` Now, for a given [a, b] interval, if `f(a)f(b) > 0` before any iteration, skip every calculation and returns a `NaN`
    instead of trying to find a root, preveting the user for misusing the method. Also, for `f(a)f(b) > 0` somewhere in
    the iteration steps, stop immediately and returns `NaN` instead of a invalid root value.
    
    - `[fix]:` Invalid `num-calc.h` location in `num-calc.c` has been updated to `lib/num-calc.h`.
    
    - `[misc]:` `lib/num-calc.h` now have a fancy organization pattern.
    
    - `[misc]:` The bisection method implementation has now comments for code readabilty.

--- lib/num-calc.h
+++ lib/num-calc.h
@@ -1,12 +1,25 @@
 #ifndef NUM_CALC_H_
 #define NUM_CALC_H_
 
+#include <stdbool.h>
+
 typedef struct {
     double a;
     double b;
 } nc_interval;
 
-typedef unsigned int uint_16;
+typedef unsigned int uint16;
+
+/*
+===== HELPER FUNCTIONS =====
+*/
+bool nc_opposite_sign(double a, double b);
+double nc_interval_size(nc_interval *interval);
+
+/*
+===== ROOT-FINDING ALGORITHM =====
+*/
+double nc_root_bisection(double f(double), nc_interval *r_interval, uint16 it);
 
 // Numerical methods for finding root
 double nc_root_bisection(double f(double), nc_interval *r_interval, uint_16 it);
--- src/num-calc.c
+++ src/num-calc.c
@@ -1,32 +1,48 @@
 #include <stdbool.h>
 #include <math.h>
-#include "num-calc.h"
+#include "../lib/num-calc.h"
 
-bool nc_opposite_sign(double a, double b) {
-    return (a * b) < 0;
-}
+/*
+===== HELPER FUNCTIONS =====
+*/
+bool nc_opposite_sign(double a, double b) {return (a * b) < 0;}
+double nc_interval_size(nc_interval *interval) {return interval->b - interval->a;}
 
-double nc_root_bisection(double f(double), nc_interval *r_interval, uint_16 it) {
+/*
+===== ROOT-FINDING ALGORITHM =====
+*/
+double nc_root_bisection(double f(double), nc_interval *r_interval, uint16 it) {
     double root = 0;
 
     double f_a = f(r_interval->a);
     double f_b = f(r_interval->b);
 
+    // It is not necessary to search for a root if the root is already the bound of
+    // the given interval.
     if(f_a == 0)
         root = r_interval->a;
 
     else if(f_b == 0)
         root = r_interval->b;
 
+    // If f(a)*f(b) > 0 for the given interval, the root is not in [a, b], thus doesn't
+    // make sense trying to compute it
+    else if(f_b * f_a > 0)
+        root = NAN;
+
     else {
-        for(uint_16 i = 0; i < it; i++) {
+        for(uint16 i = 0; i < it; i++) {
             double midpoint = (r_interval->a + r_interval->b) / 2;
             double f_midpoint = f(midpoint);
 
             if(nc_opposite_sign(f_a, f_midpoint))
                 r_interval->b = midpoint;
+
             else if(nc_opposite_sign(f_b, f_midpoint))
                 r_interval->a = midpoint;
+
+            // If f(a)*f(b) > 0 after computing the midpoint, then the root is not in
+            // the given interval.
             else {
                 root = NAN;
                 break;
